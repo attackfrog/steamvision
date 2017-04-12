a839e894637d196e449567c223a21187f039ea42
Jack McLellan <jbmclellan@protonmail.com>
2017-04-12 18:06:11 -0400

    First barebones (running?) version

--- application.py
+++ application.py
@@ -1,6 +1,12 @@
+# import os
 from flask import Flask, jsonify, render_template, request, url_for
 from flask_jsglue import JSGlue
 
 # configure application
 app = Flask(__name__)
 JSGlue(app)
+
+@app.route("/")
+def index():
+    """Display welcome page."""
+    return render_template("index.html")
