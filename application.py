f6e0fc0343b234b987eca0198a4570ca53d1f9af
Jack McLellan <jbmclellan@protonmail.com>
2017-04-12 19:55:57 -0400

    steam API, round 1

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
-# import os
+import os
+import json
 from flask import Flask, jsonify, render_template, request, url_for
 from flask_jsglue import JSGlue
 
@@ -10,3 +11,14 @@ JSGlue(app)
 def index():
     """Display welcome page."""
     return render_template("index.html")
+
+@app.route("/userinfo")
+def userinfo():
+    """Get info from Steam API about a steam user."""
+
+    if not os.environ.get("API_KEY"):
+        raise RuntimeError("API Key not set.")
+
+    gamesInfo = json.load("http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={}&steamid=76561197960434622&format=json".format(os.environ.get("API_KEY")))
+
+    return gamesInfo
