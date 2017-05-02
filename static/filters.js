a11ea3ceea7d1fecec75385aacc6792b4475a3f4
Jack McLellan <jbmclellan@protonmail.com>
2017-05-02 14:14:22 -0400

    Run function for each item, not the whole group at once

--- static/filters.js
+++ static/filters.js
@@ -6,20 +6,20 @@
 
 (function ($) {
     // Filter games by category
-    function filter_games(category) {
-        $(category).click(function () {
+    function filter_games(categories) {
+        $(categories).each().click(function () {
             // Get the category's name, removing the count from the string
             var name = $(this).text();
             name.slice(name.indexOf(' ') + 1);
 
             // If the category is active, deactivate it and show all games
-            if ($(category).hasClass('active')) {
-                $(category).removeClass('active');
+            if ($(this).hasClass('active')) {
+                $(this).removeClass('active');
                 $('#accordion').children().show();
             }
             // If the category is inactive, activate it and hide games that don't fit the category
             else {
-                $(category).addClass('active');
+                $(this).addClass('active');
                 var accordion = $('#accordion');
                 accordion.children().hide();
                 accordion.find('span:contains(' + name + ')').parents('.panel').show();
