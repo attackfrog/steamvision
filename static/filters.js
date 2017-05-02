a5ac7523cf3834abe34c0bbac47f6e95291eea1a
Jack McLellan <jbmclellan@protonmail.com>
2017-05-02 14:24:17 -0400

    Use .each() properly

--- static/filters.js
+++ static/filters.js
@@ -7,24 +7,26 @@
 (function ($) {
     // Filter games by category
     function filter_games(categories) {
-        $(categories).each().click(function () {
-            // Get the category's name, removing the count from the string
-            var name = $(this).text();
-            name.slice(name.indexOf(' ') + 1);
+        $(categories).each(function () {
+            $(this).click(function() {
+                // Get the category's name, removing the count from the string
+                var name = $(this).text();
+                name.slice(name.indexOf(' ') + 1);
 
-            // If the category is active, deactivate it and show all games
-            if ($(this).hasClass('active')) {
-                $(this).removeClass('active');
-                $('#accordion').children().show();
-            }
-            // If the category is inactive, activate it and hide games that don't fit the category
-            else {
-                $(this).addClass('active');
-                var accordion = $('#accordion');
-                accordion.children().hide();
-                accordion.find('span:contains(' + name + ')').parents('.panel').show();
-            }
-        })
+                // If the category is active, deactivate it and show all games
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active');
+                    $('#accordion').children().show();
+                }
+                // If the category is inactive, activate it and hide games that don't fit the category
+                else {
+                    $(this).addClass('active');
+                    var accordion = $('#accordion');
+                    accordion.children().hide();
+                    accordion.find('span:contains(' + name + ')').parents('.panel').show();
+                }
+            });
+        });
     }
     // Activate functionality when page has loaded
     $(function () {
