94a9492142c3f1c5e19d3d2c4e4b62bf33a4bb6f
Jack McLellan <jbmclellan@protonmail.com>
2017-05-07 15:23:46 -0400

    Sort games by release year and rating

--- static/js/sortlists.js
+++ static/js/sortlists.js
@@ -19,9 +19,11 @@
                 case 'name':
                     sort_list = sort_categories_by_name(sort_list, direction);
                     break;
+
                 case 'count':
                     sort_list = sort_categories_by_count(sort_list, direction);
                     break;
+
                 default:
                     console.log(type + ' isn\'t a valid thing to sort by...');
             }
@@ -82,6 +84,15 @@
                 case 'name':
                     sort_list = sort_games_by_name(sort_list, direction);
                     break;
+
+                case 'year':
+                    sort_list = sort_games_by_year(sort_list, direction);
+                    break;
+
+                case 'rating':
+                    sort_list = sort_games_by_rating(sort_list, direction);
+                    break;
+
                 default:
                     console.log(type + ' isn\'t a valid thing to sort by...');
             }
@@ -110,6 +121,88 @@
         }
     }
 
+    function sort_games_by_year(sort_list, direction) {
+        if (direction === 'ascending') {
+            return sort_list.sort(function (a, b) {
+                // Sort the list by comparing the release year in the title
+                return Number($(a).find('.sv-quickinfo').text().slice(-4)) - Number($(b).find('.sv-quickinfo').text().slice(-4))
+            })
+        }
+        else if (direction === 'descending') {
+            return sort_list.sort(function (a, b) {
+                // Sort the list in the same way, but inverted
+                return -(Number($(a).find('.sv-quickinfo').text().slice(-4)) - Number($(b).find('.sv-quickinfo').text().slice(-4)))
+            })
+        }
+        else {
+            console.log(direction + ' isn\'t a valid direction to sort a list in.')
+        }
+    }
+
+    function sort_games_by_rating(sort_list, direction) {
+        // Get the first game's quick info summary and remove the year to get the rating summary
+        var rating_a = $(a).find('.sv-quickinfo').text();
+        rating_a = rating_a.slice(0, rating_a.indexOf('|') - 1);
+
+        // Do the same for the second game
+        var rating_b = $(b).find('.sv-quickinfo').text();
+        rating_b = rating_b.slice(0, rating_b.indexOf('|') - 1);
+
+        if (direction === 'ascending') {
+            return sort_list.sort(function (a, b) {
+                // Sort the list by comparing the release year in the title
+                return convert_rating_to_value(rating_a) - convert_rating_to_value(rating_b)
+            })
+        }
+        else if (direction === 'descending') {
+            return sort_list.sort(function (a, b) {
+                // Sort the list in the same way, but inverted
+                return -(convert_rating_to_value(rating_a) - convert_rating_to_value(rating_b))
+            })
+        }
+        else {
+            console.log(direction + ' isn\'t a valid direction to sort a list in.')
+        }
+    }
+
+    function convert_rating_to_value(rating) {
+        // For use in a sort function, rank Steam review summaries by positivity/negativity
+        switch (rating) {
+            case 'Overwhelmingly Positive':
+                return 4;
+
+            case 'Very Positive':
+                return 3;
+
+            case 'Positive':
+                return 2;
+
+            case 'Mostly Positive':
+                return 1;
+
+            case 'Mixed':
+                return 0;
+
+            case 'Mostly Negative':
+                return -1;
+
+            case 'Negative':
+                return -2;
+
+            // I'm not sure if the next two summaries exist, but might as well handle them in case they do
+            case 'Very Negative':
+                return -3;
+
+            case 'Overwhelmingly Negative':
+                return -4;
+
+            // If provided some other value, rank it below everything else
+            default:
+                return -5;
+        }
+    }
+
+    // Activate sort buttons on page load
     $(function () {
         var category_sort_buttons = $('[aria-label="category-sort"]');
         var game_sort_buttons = $('[aria-label="game-sort"]');
@@ -121,6 +214,10 @@
 
         sort_games(game_sort_buttons.children('.sv-sort-alpha-asc'), 'name', 'ascending');
         sort_games(game_sort_buttons.children('.sv-sort-alpha-desc'), 'name', 'descending');
+        sort_games(game_sort_buttons.children('.sv-sort-year-asc'), 'year', 'ascending');
+        sort_games(game_sort_buttons.children('.sv-sort-year-desc'), 'year', 'descending');
+        sort_games(game_sort_buttons.children('.sv-sort-rating-asc'), 'rating', 'ascending');
+        sort_games(game_sort_buttons.children('.sv-sort-rating-desc'), 'rating', 'descending');
     })
 
 }(jQuery));
